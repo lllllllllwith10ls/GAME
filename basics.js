68a255f8756b709f11adf424323540a68021c1cd
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-10-07 15:17:35 -0500

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,47 @@
-
+const updateGame = () => {
+	theGame.clear();
+	if (keys[87]) {
+		if(player.vY < -5) {
+			player.vY = -5
+		} else {
+			 player.vY -= 0.1;
+		}
+	} 
+	if (keys[65]) {
+		if(player.vX < -5) {
+			player.vX = -5
+		} else {
+			 player.vX -= 0.1;
+		}
+	} 
+	if (keys[83]) {
+		if(player.vY > 5) {
+			player.vY = 5
+		} else {
+			 player.vY += 0.1;
+		}
+	}
+	if (keys[68]) {
+		if(player.vX > 5) {
+			player.vX = 5
+		} else {
+			 player.vX += 0.1;
+		}
+	}
+	if (player.xPos < 0) {
+		player.xPos = 0;
+		player.vX = -player.vX * 1/2;
+	}
+	if (player.xPos > 400) {
+		player.xPos = 400;
+		player.vX = -player.vX * 1/2;
+	}
+	if (player.yPos < 0) {
+		player.yPos = 0;
+		player.vY = -player.vY * 1/2;
+	}
+	if (player.yPos > 400) {
+		player.yPos = 400;
+		player.vY = -player.vY * 1/2;
+	}
+}
