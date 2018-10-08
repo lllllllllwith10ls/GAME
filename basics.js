4e953b84d5d4200243cb9a17b3ca400ad68a2bac
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-10-08 16:11:18 -0500

    Update basics.js

--- basics.js
+++ basics.js
@@ -4,7 +4,7 @@ class Vector {
 		this.y = y;
 	}
 	get abs() {
-		return Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+		return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
 	};
 	
 	static add(a,b) {
