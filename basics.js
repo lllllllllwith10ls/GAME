75f1c046a5e232f0b5928648d21eae67b732404d
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-10-08 18:18:50 -0500

    Update basics.js

--- basics.js
+++ basics.js
@@ -228,7 +228,7 @@ class Spaceship extends Entity {
 		}
 		this.shoot = function() {
 			if(this.cooldown >= this.reload) {
-				let angle = this.angle; 
+				let angle = this.angle+Math.PI; 
 				entities.push(new Bullet(this.pos.x,this.pos.y,-Math.sin(angle)*5,-Math.cos(angle)*5));
 				this.cooldown -= this.reload;
 			}
