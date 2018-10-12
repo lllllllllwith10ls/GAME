a0384227fa8d07c87bd5963b947a6bf3df940c7c
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-10-11 20:20:17 -0500

    Update basics.js

--- basics.js
+++ basics.js
@@ -88,7 +88,7 @@ let player = {
 			player.v.y = -player.v.y * 1/2;
 		}
 		if (this.v.abs > 2) {
-			let angle = -Math.atan2(this.v.y-this.pos.y,this.v.x-this.pos.x)-Math.PI/2;
+			let angle = -Math.atan2(this.pos.y-this.v.y,this.pos.x-this.v.x)-Math.PI/2;
 			this.v.x = Math.sin(angle)*2
 			this.v.y = Math.cos(angle)*2
 
@@ -134,7 +134,7 @@ class Entity {
 	}
 }
 Entity.prototype.update = function() {
-	if (this.v.abs > 2) {
+	if (this.v.abs > this.accel) {
 		let angle = -Math.atan2(this.v.y-this.pos.y,this.v.x-this.pos.x)-Math.PI/2;
 		this.v.x = Math.sin(angle)*2
 		this.v.y = Math.cos(angle)*2
