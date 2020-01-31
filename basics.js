39f712d2a8e31a31fd29364c736759c64417578f
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2020-01-31 11:31:51 -0600

    Update basics.js

--- basics.js
+++ basics.js
@@ -1572,12 +1572,12 @@ class BigBoi extends Entity {
 			if(this.cooldown >= this.reload) {
 				let angle = Math.PI+(Math.random()-0.5)*Math.PI; 
 				new LaserThing(this.pos.x+this.gunPos.x,this.pos.y+this.gunPos.y,-Math.sin(angle)*5,-Math.cos(angle)*5);
-				this.cooldown -= 10;
+				this.cooldown -= 5;
 			}
 			if(this.cooldown2 >= this.reload2) {
 				let angle = Math.PI*+(Math.random()-0.5)*Math.PI; 
 				new LaserThing(this.pos.x+this.gunPos2.x,this.pos.y+this.gunPos2.y,-Math.sin(angle)*5,-Math.cos(angle)*5);
-				this.cooldown2 -= 10;
+				this.cooldown2 -= 5;
 			}
 		}
 		this.charge = function() {
